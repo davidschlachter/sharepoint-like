47cb1c61a47e4dbc570ae80679bd7b923f887c7e
David Schlachter <david+gh@schlachter.ca>
2016-02-05 15:52:40 -0500

    Add Like model

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,16 @@ var express = require('express'),
 	mongoose = require('mongoose'),
 	router = express.Router();
 
+// Define the 'likes' model
+var likeModel = new mongoose.Schema({
+	userid: String,
+	postid: String,
+	timestamp: {
+		type: Date,
+		default: Date.now
+	}
+});
+var Like = mongoose.model('Like', likeModel);
 
 // GET home page.
 router.get('/', function (req, res, next) {
