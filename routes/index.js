9a5143776e4f170ab6ea92f51a1c29add2caa4bc
David Schlachter <david+gh@schlachter.ca>
2016-02-29 16:29:56 -0500

    Add comment routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var likeController = require('../controllers/likes');
+var commentController = require('../controllers/comments');
 
 // Add a new like
 router.post('/like', likeController.addLike);
@@ -11,5 +12,12 @@ router.get('/like', likeController.addLike);
 router.post('/getLikes', likeController.getLikes);
 router.get('/getLikes', likeController.getLikes);
 
+// Add a new comment
+router.post('/comment', commentController.addComment);
+router.get('/comment', commentController.addComment);
+
+// Get the current comments
+router.post('/getComments', commentController.getComments);
+router.get('/getComments', commentController.getComments);
 
 module.exports = router;
